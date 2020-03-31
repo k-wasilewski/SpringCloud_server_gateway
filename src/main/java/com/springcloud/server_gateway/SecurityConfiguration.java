6bc99592f3a304a0a0457b4adc35e11fb39c94f6
k-wasilewski <k.k.wasilewski@gmail.com>
2020-03-31 13:56:14 +0200

    initial commit

---
+++ src/main/java/com/springcloud/server_gateway/SecurityConfiguration.java
@@ -0,0 +1,38 @@
+package com.springcloud.server_gateway;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER")
+                .and()
+                .withUser("admin").password("admin").roles("ADMIN");
+
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/book-service/books").permitAll()
+                .antMatchers("/eureka/**").hasRole("ADMIN")
+                .anyRequest().authenticated().and()
+                .formLogin().and()
+                .logout().permitAll().and()
+                .csrf().disable();
+    }
+}
---
+++ src/main/java/com/springcloud/server_gateway/ServerGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.springcloud.server_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+public class ServerGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerGatewayApplication.class, args);
+    }
+
+}
