5d799f44e1d4c768e142255614b7476b7163ea42
k-wasilewski <k.k.wasilewski@gmail.com>
2020-04-06 11:27:34 +0200

    enabling as eureka client

--- src/main/java/com/springcloud/server_gateway/ServerGatewayApplication.java
+++ src/main/java/com/springcloud/server_gateway/ServerGatewayApplication.java
@@ -3,12 +3,13 @@ package com.springcloud.server_gateway;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class ServerGatewayApplication {
 
     public static void main(String[] args) {
